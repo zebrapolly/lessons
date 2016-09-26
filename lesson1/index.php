217a28598c2aabcab706d57b0d2c94afa028801f
Smirnov Mikhail <zebrapolly@gmail.com>
2016-09-26 07:36:38 +0300

    add 1 lesson

---
+++ lesson1/index.php
@@ -0,0 +1,10 @@
+<html>
+	<body id="">
+		<a href="1.html">first</a>
+		<a href="2.html">second</a>
+		<a href="3.html">third</a>
+		<a href="http://ya.ru">Yandex</a>
+		<a href="http://google.com" target="_blank">Google</a>
+		
+	</body>
+</html>
